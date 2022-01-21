61fc14c5c330d385133c81683e104952a9d159bc
DwIgor <igormv15@gmail.com>
2022-01-21 14:18:29 -0300

    task 3 - CRUd Tabela e Relacionamento

--- BlogPessoal/src/main/java/org/Generation/BlogPessoal/model/Postagem.java
+++ BlogPessoal/src/main/java/org/Generation/BlogPessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "postagem")
@@ -31,6 +34,12 @@ public class Postagem {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
 
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	
+	
 	public long getId() {
 		return id;
 	}
@@ -63,5 +72,12 @@ public class Postagem {
 		this.date = date;
 	}
 
-	
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
 }
---
+++ BlogPessoal/src/main/java/org/Generation/BlogPessoal/repository/TemaRepository.java
@@ -0,0 +1,11 @@
+package org.Generation.BlogPessoal.repository;
+
+import java.util.List;
+
+import org.Generation.BlogPessoal.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaRepository  extends JpaRepository<Tema, Long> {
+	public List<Tema> findAllByDescricaocontainingIgnoreCase( String descricao);
+
+}
